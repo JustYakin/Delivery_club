cc80f57e6426ec6bd6b7ed058aae4cd507730731
Anastasia Aleksandrova <work.aaleks@gmail.com>
2021-12-18 00:59:47 +0300

    delete main.html, add init file

---
+++ src/components/_main.js
@@ -0,0 +1,7 @@
+import './base/base';
+import './basket/basket';
+import './cards/cards';
+import './food_cards/food_cards';
+import './footer/footer';
+import './header/header';
+import './map/map';
