b071b0a1d57626ca955bdf308728c13db5dc8eee
JustYakin <yuramenkov111@gmail.com>
2022-01-14 16:09:59 +0300

    last changes

---
+++ src/components/header/header.js
@@ -0,0 +1,8 @@
+// let search_button = document.querySelector('.search__button');
+// let search_input = document.querySelector('.searsh__input');
+//
+// console.log(search_button);
+//
+// search_button.addEventListener('click', ()=> {
+//     deleteRestaraunts();
+// })
